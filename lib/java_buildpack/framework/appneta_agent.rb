15fe0abae0d4d89fae3ffeb73fd96ae84206da48
Lingesh M <lingesh.m@happiestminds.com>
2016-03-15 16:33:56 +0530

    appneta related changes

--- lib/java_buildpack/framework/appneta_agent.rb
+++ lib/java_buildpack/framework/appneta_agent.rb
@@ -22,7 +22,7 @@ require 'java_buildpack/util/qualify_path'
 module JavaBuildpack
   module Framework
 
-    # Encapsulates the functionality for enabling AppNetaAgent support.
+    # Encapsulates the functionality for enabling zero-touch Dynatrace support.
     class AppNetaAgent < JavaBuildpack::Component::VersionedDependencyComponent
       include JavaBuildpack::Util
 
@@ -31,14 +31,14 @@ module JavaBuildpack
         download_zip false
         @droplet.copy_resources
         FileUtils.mkdir(home_dir)
-        FileUtils.mv(@droplet.sandbox, home_dir)
+        FileUtils.mv(@droplet.sandbox + 'agent/linux-x86-64/agent', home_dir)
         delete_extra_files
       end
 
       # (see JavaBuildpack::Component::BaseComponent#release)
       def release
-       # @droplet.java_opts
-        #        .add_agentpath_with_props(agent_dir + "libdtagent.so", name: "Tomcat_Monitoring", server: server)
+        @droplet.java_opts
+                .add_agentpath_with_props(agent_dir + "libdtagent.so", name: "Tomcat_Monitoring", server: server)
         #this below will be used when we go for generic profile..
         #@droplet.java_opts
           #.add_agentpath_with_props(agent_dir + 'libdtagent.so',
@@ -50,35 +50,34 @@ module JavaBuildpack
 
       # (see JavaBuildpack::Component::VersionedDependencyComponent#supports?)
       def supports?
-        true 
-        #@application.services.one_service? FILTER, 'server'
+        @application.services.one_service? FILTER, 'server'
       end
 
       private
 
-      #FILTER = /appneta/.freeze
+      FILTER = /appneta/.freeze
 
-      #private_constant :FILTER
+      private_constant :FILTER
 
       def application_name
         @application.details['application_name']
       end
 
       def profile_name
-       # @application.services.find_service(FILTER)['credentials']['profile'] || 'Monitoring'
+        @application.services.find_service(FILTER)['credentials']['profile'] || 'Monitoring'
       end
 
       def agent_dir
-        #@droplet.sandbox + 'home/agent/lib64'
+        @droplet.sandbox + 'home/agent/lib64'
       end
 
       def delete_extra_files
-        #FileUtils.rm_rf(@droplet.sandbox + 'agent')
-        #FileUtils.rm_rf(@droplet.sandbox + 'init.d')
-        #ileUtils.rm_rf(@droplet.sandbox + 'com')
-        #FileUtils.rm_rf(@droplet.sandbox + 'org')
-        #FileUtils.rm_rf(@droplet.sandbox + 'META_INF')
-        #FileUtils.rm_f(@droplet.sandbox + 'YouShouldNotHaveUnzippedMe.txt')
+        FileUtils.rm_rf(@droplet.sandbox + 'agent')
+        FileUtils.rm_rf(@droplet.sandbox + 'init.d')
+        FileUtils.rm_rf(@droplet.sandbox + 'com')
+        FileUtils.rm_rf(@droplet.sandbox + 'org')
+        FileUtils.rm_rf(@droplet.sandbox + 'META_INF')
+        FileUtils.rm_f(@droplet.sandbox + 'YouShouldNotHaveUnzippedMe.txt')
       end
 
       def logs_dir
@@ -90,7 +89,7 @@ module JavaBuildpack
       end
 
       def server
-        #@application.services.find_service(FILTER)['credentials']['server']
+        @application.services.find_service(FILTER)['credentials']['server']
       end
 
     end
